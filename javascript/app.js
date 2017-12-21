239d1b4c1c2384b70e8d6936a8a809ddb67ad43b
Henry Mesias <henrymesias@gmail.com>
2017-12-20 21:20:17 -0500

    udpated index file with jquery, bootstrap, and app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,24 @@
 
 $(document).ready(function(){
 
-$('#search').on('click', function(){
-
+$('#search').on('click', function(e){
+e.preventDefault();
     var searchTerm = $('#searchTerm').val();
     var api_key = 'ec82c8de63674fb6bcec39cab6b828d3';
     var queryUrl =  'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + api_key + "&q=" + searchTerm;
 
+   
 
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).done(function(response) {
 
-
-
+        console.log(response);
+      
+    
+    
+    });
 });
 
 // end
